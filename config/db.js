5aae4c1c5fd58bfee28c6e771fc0672b0004f57f
Athira.V.Ajit <mailathiravajit@gmail.com>
2020-09-24 11:44:41 +0530

    added signup page

--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
 const mysql=require('mysql');
 
 function connectDB(){
-    pool=mysql.createPool({
+    con=mysql.createConnection({
         host:"localhost",
         user:"root",
         password:"",
         database:"snaptok"
     });
+    con.connect(function(err){
+        if(err) throw err;
+        console.log("connected");
+    })
 }
 
 function response(s){
     return new Promise(function(resolve,reject){
-        pool.query(s,function(err,result,fields){
+        con.query(s,function(err,result,fields){
             if(err){return reject(err);}
             resolve(result);
         })
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const {response} = require('../config/db')
+
+router.post('/signup',(req,res)=>{
+    s =  `SELECT * FROM users WHERE email='${req.body.email}'`
+    response(s)
+        .then((user)=>{
+            console.log(user)
+            if( user.length == 0 ){
+                s = `INSERT INTO users (email,name,bio,age,password) VALUES ('${req.body.email}','${req.body.name}','${req.body.bio}','${req.body.age}','${req.body.password}')`
+                response(s)
+                    .then(()=>{
+                        console.log("done")
+                    })
+            }
+            else{
+                res.redirect('/?message=user exists')
+            }
+        })
+        .catch((err)=>{
+            console.log("error");
+            throw err;
+        })
+})
+
+
+
 module.exports = router
